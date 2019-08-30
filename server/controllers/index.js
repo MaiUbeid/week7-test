ae614de4716df43498167389d1bfcb8abf27c7bc
MaiUbeid <ubead95@gmail.com>
2019-08-30 21:11:30 +0300

    build database schema and connect it

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,9 +2,4 @@ const express = require('express');
 
 const router = express.Router();
 
-const {getData, postData} = require('./cities');
-
-router.get('/cities', getData);
-router.post('/add-city', postData);
-
 module.exports = router;
